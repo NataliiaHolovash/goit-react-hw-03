6675f4a0b074127e2be8fedf368ebf06368ebedc
NataliiaHolovash <n.golovash@ukr.net>
2025-03-09 15:56:45 +0100

    update my hw

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,25 +1,90 @@
-import css from './ContactForm.module.css'
+import css from "./ContactForm.module.css";
 
-export default function ContactForm() {
+import { useId } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { nanoid } from "nanoid";
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log('handleSubmit');
-    }
+const ContactForm = ({ onAdd }) => {
+  const nameFieldId = useId();
+  const numberFieldId = useId();
 
-    return (
-        <form className={css["form"]} onSubmit={handleSubmit} >
-            <div className={css["input-group"]}>
-                <label>Name</label>
-                <input type="text" name="username" required />
-            </div>
+  const initialValues = {
+    username: "",
+    number: "",
+  };
 
-            <div className={css["input-group"]}>
-                <label>Number</label>
-                <input type="number"name="number" required />
-            </div>
+  const validationSchema = Yup.object().shape({
+    username: Yup.string()
+      .required("Required!")
+      .min(3, "Too short!")
+      .max(50, "Name must be at most 50 characters!")
+      .test(
+        "trim",
+        "Name cannot be only spaces!",
+        (value) => value.trim() !== ""
+      ),
+    number: Yup.string()
+      .required("Required!")
+      .min(3, "Too short!")
+      .max(50, "Number must be at most 50 characters!")
+      .matches(/^[0-9-]+$/, "Number must contain only digits and -"),
+  });
 
-            <button type="submit">Add contact </button>
-        </form>
-    )
-}
+  const handleSubmit = (values, actions) => {
+    const newContact = {
+      id: nanoid(),
+      name: values.username,
+      number: values.number,
+    };
+
+    onAdd(newContact);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form className={css.form}>
+        <div className={css.formElement}>
+          <label htmlFor={nameFieldId} className={css.label}>
+            Name
+          </label>
+          <Field
+            type="text"
+            name="username"
+            id={nameFieldId}
+            className={css.input}
+          />
+          <ErrorMessage
+            name="username"
+            component="span"
+            className={css.warning}
+          />
+        </div>
+        <div className={css.formElement}>
+          <label htmlFor={numberFieldId}>Number</label>
+          <Field
+            type="text"
+            name="number"
+            id={numberFieldId}
+            className={css.input}
+          />
+          <ErrorMessage
+            className={css.warning}
+            name="number"
+            component="span"
+          />
+        </div>
+        <button type="submit" className={css.btn}>
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default ContactForm;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "modern-normalize";
+import "./index.css";
 import App from './Components/App/App'
 
 createRoot(document.getElementById('root')).render(
